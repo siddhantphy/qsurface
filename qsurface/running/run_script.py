b7afda1751a3f6aacee52ff3688be50efafbac0d
Siddhant Singh <ssingh8@tudelft.nl>
2022-01-18 13:50:53 +0100

    Superopator add

---
+++ qsurface/running/run_script.py
@@ -0,0 +1,13 @@
+from qsurface.main import initialize, run, BenchmarkDecoder
+from qsurface.decoders import mwpm
+
+code = initialize((5,5), "toric", "mwpm", superoperator_enabled=True, Superoperator="abcd")
+print(code)
+
+# run(code, decoder, iterations=10, error_rates = {"p_bitflip": 0.1})
+
+import sys
+print(sys.path)
+
+import qsurface
+print(qsurface.__file__)
